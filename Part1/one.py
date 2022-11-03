620236e75607189db00d6a3f49d066d8f2daaab7
Nikanorik <nikanorov.company@gmail.com>
2022-11-03 19:37:44 +0300

    New file

---
+++ Part1/one.py
@@ -0,0 +1,20 @@
+def is_isogram(text):
+    a=-len(text)
+    for i in range(len(text)):
+        num = text[i]
+
+        for two in text:
+
+            if num.lower() == two.lower() or num==two!='':
+                a+=1
+            else:
+                a+=0
+    if a > 0:
+        print("same chars may not be adjacent")
+        return False
+
+    else:
+        print(True)
+        return True
+
+is_isogram("")
