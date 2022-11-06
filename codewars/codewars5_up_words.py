a2be0eeb3e6162c7879d38679ae094e0fc23b8b8
Nikanorik <nikanorov.company@gmail.com>
2022-11-06 14:00:10 +0300

    New file

--- codewars/codewars5_up_words.py
+++ codewars/codewars5_up_words.py
@@ -1,9 +1,11 @@
 def to_jaden_case(string):
-    text_new = string.split()
-    text_one=''
-    for i in text_new:
-        i[0]=i[0].upper()
-        text_one+=i
+    new=string.title()
+    if "'" in new:
+        ber = new.find("'")
+    new_one = new.replace(new[ber+1],new[ber+1].lower())
+
+    print(new_one)
+
 
 
 to_jaden_case("How can mirrors be real if our eyes aren't real")
