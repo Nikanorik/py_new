d9d05696c3fa5da155f6d217d6e3daec0c50b6b0
Nikanorik <nikanorov.company@gmail.com>
2022-11-07 18:03:58 +0300

    New file operator

---
+++ codewars/codewars9.py
@@ -0,0 +1,14 @@
+def basic_op(operator, value1, value2):
+    if operator == '+':
+        return value1+value2
+    elif operator =='-':
+        return value1-value2
+    elif operator == '*':
+        return value1*value2
+    elif operator == '/':
+        return  value1/value2
+
+
+
+
+basic_op('+', 15, 18)
