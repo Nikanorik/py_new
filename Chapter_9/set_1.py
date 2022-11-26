4a6d3309f8d74538e1ae864e6e9d697fa3bc007a
Nikanorik <nikanorov.company@gmail.com>
2022-11-26 15:12:56 +0300

    Add file pickle

--- Chapter_9/set_1.py
+++ Chapter_9/set_1.py
@@ -1,3 +1,22 @@
-myset = set([10,9,8])
-myset.update('abc')
-print(myset)
+import pickle
+def main():
+    file = open('test.dat','wb')
+    flag = 'y'
+    while flag=='y':
+        repeat_print(file)
+        flag = input('Do you want to next?(y/n) ').lower()
+    file.close()
+
+def repeat_print(file):
+    person={}
+    person['name']=input('Enter name: ')
+    person['age']= input('Enter age: ')
+    person['country'] = input('Enter country: ')
+    pickle.dump(person,file)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
