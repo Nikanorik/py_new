343873e3c14617c1c6b23e86c16a63289d81d970
Nikanorik <nikanorov.company@gmail.com>
2022-12-18 21:30:10 +0300

    Add file new_codewars4

---
+++ codewars/long_name.py
@@ -0,0 +1,14 @@
+def friend(x):
+    list=[]
+    for i in x:
+        long=len(i)
+        if long==4:
+            list.append(i)
+    return list
+
+
+
+
+
+
+friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"])
