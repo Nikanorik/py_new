22578b9c305da4431c9d13f4a69e4b3613b71ad0
Nikanorik <nikanorov.company@gmail.com>
2022-12-21 16:29:21 +0300

    Add file codewars_21.12.1

--- codewars/codewars_21.12.py
+++ codewars/codewars_21.12.py
@@ -1,11 +1,11 @@
 def quarter_of(month):
     # your code here
     month_list=[1,2,3,4,5,6,7,8,9,10,11,12]
-    if month in month_list[1:4]:
+    if month in month_list[0:3]:
         return 1
-    elif month in month_list[4:7]:
+    elif month in month_list[3:6]:
         return 2
-    elif month in month_list[7:10]:
+    elif month in month_list[6:9]:
         return 3
     else:
         return 4
