31a191409db00508b0557de1675c3f5ee9eb0a9f
Nikanorik <nikanorov.company@gmail.com>
2022-12-21 18:26:02 +0300

    Add file codewars_21.12.5

---
+++ codewars/codewars_21_12_2.py
@@ -0,0 +1,18 @@
+def high(x):
+    # Code here
+    alphavit='abcdefghijklmnopqrstuvwxyz'
+    list=x.split()
+    list_n=[]
+
+    for i in list:
+        sum = 0
+        for a in i:
+            number=alphavit.index(a)+1
+            sum+=number
+        list_n.append(sum)
+
+    return list[list_n.index(max(list_n))])
+
+
+
+high('man i need a taxi up to ubud')
