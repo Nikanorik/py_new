2b7193e1a5d5c675dde37ffc8c3d14652f588677
Nikanorik <nikanorov.company@gmail.com>
2022-12-27 16:04:33 +0300

    Add file codewars_27_12_1**

--- codewars/codewars_26_12_3.py
+++ codewars/codewars_26_12_3.py
@@ -1,26 +1,17 @@
 def max_sequence(arr):
     print(arr)
-    count=0
-    for b in arr:
-        if b<0:
-            count+=1
-            if count==len(arr):
-                return 0
-    if arr==[]:
+    list = []
+    summa = 0
+    for a in range(len(arr)):
+        summa += arr[a]
+        if summa>=0:
+            list.append(summa)
+        else:
+            summa=0
+    if list == [] or arr == []:
         return 0
     else:
-        list = []
-        for i in range(len(arr)):
-            summa = 0
-            for a in range(i,len(arr)):
-                summa+=arr[a]
-                if summa > arr[a]:
-                    list.append(summa)
         return max(list)
 
 
-
-
-
-
-max_sequence([-2, -1, -3, -4, -1, -2, -1, -5, -4])
+max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
